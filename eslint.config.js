b994ab11b65e825586342449da433bb5ae40da03
ryanwolhuter <dev@ryanwolhuter.com>
2025-09-18 13:34:01 +0200

    feat: update configs
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,70 +1,135 @@
 // @ts-check
 import { default as eslint } from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig } from "eslint/config";
 import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
+import { globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 
-export default tseslint.config(
-  globalIgnores([
-    "node_modules/",
-    "dist/",
-    ".ph/",
-    "eslint.config.js",
-  ]),
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      sourceType: "module",
-      ecmaVersion: "latest",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
+/** All of the files that are ignored by eslint */
+const ignoredFiles = [
+  "**/node_modules/",
+  "**/coverage/",
+  "**/dist/",
+  "**/.ph/",
+  "**/storybook-static/",
+  "**/.vite/",
+  "**/build/",
+];
+
+/** Global configs for eslint ignores */
+const ignored = globalIgnores(ignoredFiles);
+
+/** Typescript (`.ts`) files */
+const typescriptFiles = ["**/*.ts"];
+
+/** Typescript React (`.tsx`) files */
+const typescriptReactFiles = ["**/*.tsx"];
+
+/** Javascript (`.js`, `.cjs`, `.mjs`) files */
+const javascriptFiles = ["**/*.js", "**/*.cjs", "**/*.mjs"];
+
+/** Typescript rules that we have chosen to opt out of in general */
+/** @type {import("eslint").Linter.RulesRecord} */
+const typescriptRules = {
+  "@typescript-eslint/consistent-type-imports": [
+    "error",
+    {
+      prefer: "type-imports",
+      disallowTypeAnnotations: true,
+      fixStyle: "separate-type-imports",
     },
-    rules: {      
-      "@typescript-eslint/consistent-type-imports": [
-      "error",
-      {
-        disallowTypeAnnotations: false,
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/prefer-nullish-coalescing": "off",
-      "@typescript-eslint/no-floating-promises": "off",
-      "@typescript-eslint/no-misused-promises": "off",
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-duplicate-type-constituents": "off",
-      "@typescript-eslint/no-unused-vars": "off",
+  ],
+  "@typescript-eslint/no-explicit-any": "off",
+  "@typescript-eslint/no-unused-vars": "warn",
+  "@typescript-eslint/no-unnecessary-condition": "warn",
+  "@typescript-eslint/require-await": "warn",
+  "@typescript-eslint/no-misused-promises": "warn",
+  "@typescript-eslint/no-floating-promises": "warn",
+  "@typescript-eslint/no-empty-object-type": "warn",
+  "@typescript-eslint/no-duplicate-type-constituents": "warn",
+  "no-constant-condition": "off",
+  "@typescript-eslint/restrict-template-expressions": [
+    "warn",
+    {
+      allowNumber: true,
+    },
+  ],
+  "@typescript-eslint/no-unnecessary-type-assertion": "off",
+  "@typescript-eslint/no-unnecessary-type-parameters": "off",
+};
+
+/** Language options for typescript files 
+@type {import("eslint").Linter.LanguageOptions} */
+const typescriptLanguageOptions = {
+  sourceType: "module",
+  ecmaVersion: "latest",
+  globals: {
+    ...globals.browser,
+    ...globals.node,
+  },
+  parserOptions: {
+    projectService: {
+      allowDefaultProject: ["eslint.config.js"],
+    },
+    tsconfigRootDir: import.meta.dirname,
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-  {
-    files: ["**/*.tsx"],
-    ...reactPlugin.configs.flat.recommended,
-    ...reactPlugin.configs.flat["jsx-runtime"],
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    plugins: {
-      react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
-    },
-  }
+};
+
+/** React plugins */
+const reactPlugins = {
+  react: reactPlugin,
+  "react-hooks": reactHooksPlugin,
+};
+
+/** React settings */
+const reactSettings = {
+  react: {
+    version: "detect",
+  },
+};
+
+/** Typescript config for both `.ts` and `.tsx` files */
+const typescriptConfig = {
+  files: [...typescriptFiles, ...typescriptReactFiles],
+  languageOptions: typescriptLanguageOptions,
+  rules: typescriptRules,
+};
+
+/** React config for `.tsx` files */
+const reactConfig = {
+  files: typescriptReactFiles,
+  settings: reactSettings,
+  plugins: reactPlugins,
+};
+
+/** Config for javascript files */
+const javascriptConfig = {
+  // disable type aware linting for js files
+  files: javascriptFiles,
+  extends: [tseslint.configs.disableTypeChecked],
+};
+
+/** Recommended config from eslint */
+const eslintRecommendedConfig = eslint.configs.recommended;
+
+/** Recommended config from typescript-eslint */
+const typescriptEsLintRecommendedConfig = [
+  ...tseslint.configs.recommendedTypeChecked,
+];
+
+/** Main config */
+export default defineConfig(
+  ignored,
+  eslintRecommendedConfig,
+  typescriptEsLintRecommendedConfig,
+  eslintPluginPrettierRecommended,
+  typescriptConfig,
+  reactConfig,
+  javascriptConfig,
 );
